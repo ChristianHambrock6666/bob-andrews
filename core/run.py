6a6bc9740ae26db96613139fb49b4fae8c6607ef
chambroc <christian.hambrock@otto.de>
2018-05-17 14:46:46 +0200

    ch| add tensorboard blingbling

--- core/run.py
+++ core/run.py
@@ -17,12 +17,30 @@ import lime
 import lime.lime_text
 import lime.lime_tabular
 import datetime
-tb_dir = ".././output/tensorboard/" + datetime.datetime.now().strftime("%I:%M%p_on_%B_%d_%Y")
-os.mkdir(tb_dir)
+import threading
 
+from multiprocessing import Process
+
+
+def launchTensorBoard(path):
+    import os
+    os.system("bash -c \"source /home/chambroc/miniconda3/bin/activate bob && tensorboard --logdir=" + path + "\"")
+    return
+
+
+tb_dir = os.getcwd() + "/.././output/tensorboard/" + datetime.datetime.now().strftime("%I:%M%p_on_%B_%d_%Y")
+os.mkdir(tb_dir)
+os.mkdir(tb_dir + "/train")
+os.mkdir(tb_dir + "/test")
+os.mkdir(tb_dir + "/lime")
+p = Process(target=launchTensorBoard, args=(tb_dir,))
+p.start()
+tensorboard_train_writer = tf.summary.FileWriter(tb_dir + "/train")
+tensorboard_test_writer = tf.summary.FileWriter(tb_dir + "/test")
+tensorboard_lime_writer = tf.summary.FileWriter(tb_dir + "/lime")
 
 cf = cf.Config()
-tex_writer = TeXwriter(".././output", "doc")
+tex_writer = TeXwriter(".././output/LaTeX", "doc")
 tex_writer.addSection("Parameters")
 tex_writer.addText(cf.to_tex())
 loader = ld.Loader(cf)
@@ -32,66 +50,40 @@ trainer = tn.Trainer(cf, network)
 evaluator = ev.Evaluator(cf, network, char_trf)
 test_features, test_labels = loader.get_test_data()
 
-tf.summary.scalar("accuracy", trainer.accuracy)
-merged = tf.summary.merge_all()
-
-
 with tf.Session() as sess:
-
     sess.run(tf.global_variables_initializer())
-    train_writer = tf.summary.FileWriter('../output/test1', sess.graph)
 
     while loader.epochs < cf.epochs:
         batch_x, batch_y = loader.get_next_train_batch_sample(cf.batch_size)
-        current_output = trainer.train(sess, batch_x, batch_y, train_writer)
+        current_output = trainer.train(sess, batch_x, batch_y, tensorboard_train_writer)
 
         if loader.new_epoch:
-            current_test_output = trainer.test(sess, test_features, test_labels)
-            # TODO
+            current_test_output = trainer.test(sess, test_features, test_labels, tensorboard_test_writer)
+            print("epochs: " + str(loader.epochs))
 
-
-        trainer.print_info_()
-
-    # colorize text examples
     tex_writer.addSection("Text examples")
     tex_writer.addText("""The text is colored red if the character was important for the prediction in the following sense:\n\n
     The character is removed (set to default). The prediction is thus changed. 
-    The bigger the change towards the category 'no-word-found' of the prediction, the brighter is the character colored. 
-    \\vspace{1cm}
-    \n\n
-    """)
-    feature_names = [str(num) for num in range(200)]
-    categorical_features = range(200)
+    The bigger the change towards the category 'no-word-found' of the prediction, the brighter is the character colored.\n\\vspace{1cm}\n\n""")
+    feature_names = [str(num) for num in range(cf.string_length)]
+    categorical_features = range(cf.string_length)
     categorical_names = [['-'] + [char_trf.num2char[ch + 1] for ch in range(len(char_trf.num2char))] for num in
-                         range(200)]
+                         range(cf.string_length)]
     train = np.array([np.array(char_trf.tensor_to_numbers(tensor)) for tensor in loader.get_next_train_batch(1000)[0]])
 
     # --------LIME:-------------------------------------------------------
-    predict_fn = lambda num_sentences: np.array([
-        np.array(evaluator.predict(sess, char_trf.numbers_to_tensor(num_sentence)))
-        for num_sentence in num_sentences])
-
-
-    def predict_text_fn(txt_sentence):
-        ss = txt_sentence
-        while len(ss) < 200:
-            ss = ss + " "
-        ret = np.array(evaluator.predict(sess, char_trf.string_to_tensor(ss)))
-        return ret
-
-    predict_ss_fn = lambda txt_sentences: np.array([predict_text_fn(txt_sentence) for txt_sentence in txt_sentences])
-    explainer = lime.lime_tabular.LimeTabularExplainer(train, class_names=['absent', 'contained'],
-                                                       feature_names=feature_names,
-                                                       categorical_features=categorical_features,
-                                                       categorical_names=categorical_names, kernel_width=None,
-                                                       verbose=False)
-    explainer_text = lime.lime_text.LimeTextExplainer(
-        split_expression=r'\W+'
-        # bow=False  # default True: then words will loose order and double words will be removed as I understand
-    )
-
-    text = sess.run(tf.summary.text('tag1', tf.convert_to_tensor('Tag1: Random Text 1' + str(loader.batches))))
-    train_writer.add_summary(text)
+    predict_table_fn = lambda num_sentences: np.array(
+        [np.array(evaluator.predict(sess, char_trf.numbers_to_tensor(num_sentence))) for num_sentence in num_sentences])
+    explainer_table = lime.lime_tabular.LimeTabularExplainer(train,
+                                                             class_names=['absent', 'contained'],
+                                                             feature_names=feature_names,
+                                                             categorical_features=categorical_features,
+                                                             categorical_names=categorical_names, kernel_width=None,
+                                                             verbose=False)
+    predict_text_fn = lambda txt_sentence: np.array(
+        evaluator.predict(sess, char_trf.string_to_tensor(char_trf.string_to_const_length(txt_sentence))))
+    predict_texts_fn = lambda txt_sentences: np.array([predict_text_fn(txt_sentence) for txt_sentence in txt_sentences])
+    explainer_text = lime.lime_text.LimeTextExplainer(split_expression=r'\W+')
 
     for tensor_sentence, truth in zip(test_features[50:100], test_labels[50:100]):
         sentence = char_trf.tensor_to_string(tensor_sentence).replace("-", " ")
@@ -111,9 +103,9 @@ with tf.Session() as sess:
 
         # --------LIME Table:-------------------------------------------------------
         tex_writer.addText("\n\n")
-        char_importances_lime = explainer.explain_instance(
+        char_importances_lime = explainer_table.explain_instance(
             np.array(char_trf.tensor_to_numbers(tensor_sentence)),
-            predict_fn, num_features=5)
+            predict_table_fn, num_features=5)
 
         dic = dict(char_importances_lime.as_map()[1])
         sum_importance = sum([abs(v) for v in dic.values()])
@@ -132,13 +124,16 @@ with tf.Session() as sess:
         sentence = re.sub(r'\W+', " ", sentence)
         word_importances_lime = explainer_text.explain_instance(
             sentence,
-            predict_ss_fn,
+            predict_texts_fn,
             num_features=5)
         dic = dict(word_importances_lime.as_map()[1])
         word_importance_mapping = dict(
             [(word_importances_lime.domain_mapper.indexed_string.as_list[e], dic.get(i, 0)) for i, v in
-             enumerate(word_importances_lime.domain_mapper.indexed_string.positions) for e in v])  # this is just weird mapping to unique words
-        print(word_importance_mapping)
+             enumerate(word_importances_lime.domain_mapper.indexed_string.positions) for e in v])  # mapping to unique words
+        text = sess.run(tf.summary.text('word explanation',
+                                        "sentence: \n" + sentence + "\n\nword explainer result:\n" +
+                                        tf.convert_to_tensor(str(word_importance_mapping))))
+        tensorboard_lime_writer.add_summary(text)
         sum_importance = sum([abs(v) for v in dic.values()])
         split_sentence = re.split(r'\W+', sentence)
         for idx, word in zip(range(len(split_sentence)), split_sentence):
@@ -156,3 +151,4 @@ with tf.Session() as sess:
 
 tex_writer.compile()
 subprocess.call(["xdg-open", tex_writer.outputFile])
+p.terminate()
--- core/trainer.py
+++ core/trainer.py
@@ -31,12 +31,9 @@ class Trainer(object):
         self.accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
 
     def _initialize_tensorboard(self):
-        tf.summary.scalar("train_accuracy", self.accuracy)
-        tf.summary.scalar("train_cost", self.cost)
-        tf.summary.scalar("test_accuracy", self.accuracy)
-        tf.summary.scalar("test_cost", self.cost)
-        self.train_merged = tf.summary.merge_all()
-        self.test_merged = tf.summary.merge_all()
+        tf.summary.scalar("accuracy", self.accuracy)
+        tf.summary.scalar("cost", self.cost)
+        self.merged = tf.summary.merge_all()
 
     def _loss_(self, prediction, y):
         """loss function only taken out of initialize_graph because of more singular importance... private, dont touch, dont need to!"""
@@ -44,22 +41,23 @@ class Trainer(object):
         single_loss = tf.pow(prediction - y, 2)
         return tf.reduce_mean(single_loss)
 
-    def train(self, sess, batch_x, batch_y, train_writer):
+    def train(self, sess, batch_x, batch_y, tensorboard_writer):
         """run the graph build in the constructor"""
         self.batch_cnt = self.batch_cnt + 1
-        self.train_output = sess.run([self.optimizer, self.cost, self.network.weights, self.accuracy, self.train_merged], feed_dict={self.x: batch_x, self.y: batch_y})
-        train_writer.add_summary(self.train_output[4], self.batch_cnt)
+        self.train_output = sess.run([self.optimizer, self.merged], feed_dict={self.x: batch_x, self.y: batch_y})
+        tensorboard_writer.add_summary(self.train_output[1], self.batch_cnt)
         return self.train_output
 
-    def test(self, sess, x, y):
+    def test(self, sess, x, y, tensorboard_writer):
         """same as train, only do not include the optimizer to not move the weights when generating output"""
-        self.test_output = sess.run([self.cost, self.accuracy], feed_dict={self.x: x, self.y: y})
+        self.test_output = sess.run([self.merged], feed_dict={self.x: x, self.y: y})
+        tensorboard_writer.add_summary(self.test_output[0], self.batch_cnt)
+        tensorboard_writer.flush()
         return self.test_output
 
 
     def print_info_(self):
         """wip"""
         print("--------------------------------------------------------------")
-        print("cost", self.train_output[1])
-        print("batch avg accuracy: ", self.train_output[3])
+        print("merged:", self.train_output[1])
 
